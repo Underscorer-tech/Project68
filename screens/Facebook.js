af0a8faca339f8f3c6f0b139219d66480fffeaf5
Underscorer-tech <78732817+Underscorer-tech@users.noreply.github.com>
2021-09-07 18:34:24 +0530

    Add files via upload

---
+++ screens/Facebook.js
@@ -0,0 +1,25 @@
+import  * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class FaceBook extends React.Component{
+
+render(){
+
+return(
+
+<View style={{flex:1,
+justifyContent:'center',
+alignItems:'center'
+
+}}>
+
+<Text>FaceBook</Text>
+
+</View>
+
+)
+
+}
+
+}
+
---
+++ screens/Instagram.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Insta extends React.Component{
+
+render(){
+
+return(
+
+<View style={{flex:1,
+justifyContent:'center',
+alignItems:'center'
+
+}}>
+
+<Text>Instagram</Text>
+
+</View>
+
+)
+
+}
+
+}
+
+
